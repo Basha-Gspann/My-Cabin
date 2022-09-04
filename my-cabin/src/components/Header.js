c9016fe537f27c313b424e231e558875705784ca
ypushpa12 <105192839+ypushpa12@users.noreply.github.com>
2022-09-04 16:05:18 +0530

    changes

--- my-cabin/src/components/Header.js
+++ my-cabin/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Menu from './svg/bars-solid.svg'
 import Close from './svg/times-solid.svg'
-// import CartIcon from './svg/shopping-cart-solid.svg'
+import CartIcon from './svg/shopping-cart-solid.svg'
 import {Link} from 'react-router-dom'
 import './css/Header.css'
 import {DataContext} from './Context'
@@ -22,7 +22,7 @@ export class Header extends Component {
 
     render() {
         const {toggle} = this.state;
-        // const {cart} = this.context;
+        const {cart} = this.context;
         return (
             <header>
                 <div className="menu" onClick={this.menuToggle}>
@@ -43,10 +43,10 @@ export class Header extends Component {
                         </li>
                     </ul>
                     <div className="nav-cart">
-                        {/* <span>{cart.length}</span>
+                        <span>{cart.length}</span>
                         <Link to="/cart">
                             <img src={CartIcon} alt="" width="20"/>
-                        </Link> */}
+                        </Link>
                     </div>
                 </nav>
             </header>
--- my-cabin/src/components/section/Home.js
+++ my-cabin/src/components/section/Home.js
@@ -6,33 +6,33 @@ import React from 'react'
 function Home() {
   return (
     <div>
-    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-<div className="carousel-indicators">
-<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-</div>
-<div className="carousel-inner">
-<div className="carousel-item active">
- <img src="https://stylesatlife.com/wp-content/uploads/2020/01/office-cabin-interior-design.jpg" className="d-block w-100" alt="..."/>
-</div>
-<div className="carousel-item">
- <img src="https://thearchitecturedesigns.com/wp-content/uploads/2019/12/office-cabin-1.jpg" className="d-block w-100" alt="..."/>
-</div>
-<div className="carousel-item">
- <img src="https://i.pinimg.com/originals/1e/8c/95/1e8c955685931368e6ad6bd042d4df84.jpg" className="d-block w-100" alt="..."/>
-</div>
-</div>
-<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-<span className="carousel-control-prev-icon" aria-hidden="true"></span>
-<span className="visually-hidden">Previous</span>
-</button>
-<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-<span className="carousel-control-next-icon" aria-hidden="true"></span>
-<span className="visually-hidden">Next</span>
-</button>
-</div>
-   </div>
+      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="https://stylesatlife.com/wp-content/uploads/2020/01/office-cabin-interior-design.jpg" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="https://thearchitecturedesigns.com/wp-content/uploads/2019/12/office-cabin-1.jpg" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="https://i.pinimg.com/originals/1e/8c/95/1e8c955685931368e6ad6bd042d4df84.jpg" className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
   )
 }
 
